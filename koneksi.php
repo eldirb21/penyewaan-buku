bf056e7b996a5b1f00a0ae2d7c125ec08cbc4a7c
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-09-12 05:13:56 +0700

    membuat koneksi

---
+++ koneksi.php
@@ -0,0 +1,8 @@
+<?php
+
+    $server = "localhost";
+    $user = "root";
+    $password = "";
+    $database = "tokobuku";
+
+    mysqli_connect($server, $user, $password, $database) OR DIE ("koneksi Gagal");
